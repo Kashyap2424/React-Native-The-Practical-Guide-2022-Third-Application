449c8fc498e93b5bc640966b6c12cc79281d9b9f
Kashyap2424 <kapdikashyap2003@gmail.com>
2022-09-07 14:44:26 +0530

    Adding Images & Styling

--- Components/MealItem.js
+++ Components/MealItem.js
@@ -1,13 +1,70 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable, Image } from "react-native";
 
-const MealItem = ({ title }) => {
+const MealItem = ({ title, imageUrl, duration, complexity, affordability }) => {
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image style={styles.image} source={{ uri: imageUrl }} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailsItem}>{duration}M</Text>
+            <Text style={styles.detailsItem}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.detailsItem}>
+              {affordability.toUpperCase()}
+            </Text>
+          </View>
+        </View>
+      </Pressable>
     </View>
   );
 };
 
 export default MealItem;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    elevation: 4,
+    backgroundColor: "#fff",
+    shadowOffset: { width: 0, height: 1 },
+    shadowColor: "#000",
+    shadowRadius: 8,
+    shadowOpacity: 0.18,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailsItem: {
+    fontSize: 12,
+    marginHorizontal: 4,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+});
